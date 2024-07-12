028f3b9262c9fecd411d313ac06e05d169c1146d
artucorreia <115330684+artucorreia@users.noreply.github.com>
2024-07-12 20:24:45 -0300

    Initial commit

---
+++ src/main/java/com/blog/cesmusic/CesmusicApplication.java
@@ -0,0 +1,13 @@
+package com.blog.cesmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/cesmusic/CesmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.cesmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
