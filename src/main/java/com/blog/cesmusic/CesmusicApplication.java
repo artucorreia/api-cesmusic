b3db0276d635830f212d5f612fb1c32d2195819b
artucorreia <115330684+artucorreia@users.noreply.github.com>
2024-07-15 13:51:14 -0300

    swagger
    swagger implementado

--- src/main/java/com/blog/cesmusic/CesmusicApplication.java
+++ src/main/java/com/blog/cesmusic/CesmusicApplication.java
@@ -1,9 +1,24 @@
 package com.blog.cesmusic;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info =
+		@Info(
+				title = "API Cesmusic",
+				version = "v1",
+				description = "API REST Full utilizada para o projeto Cesmusic",
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				)
+		)
+)
 public class CesmusicApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/blog/cesmusic/controllers/AuthenticationController.java
+++ src/main/java/com/blog/cesmusic/controllers/AuthenticationController.java
@@ -8,6 +8,12 @@ import com.blog.cesmusic.exceptions.auth.LoginAlreadyUsedException;
 import com.blog.cesmusic.model.User;
 import com.blog.cesmusic.services.auth.TokenService;
 import com.blog.cesmusic.services.auth.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Authentication", description = "Endpoints for registration and login to the system")
 public class AuthenticationController {
 
     @Autowired
@@ -37,6 +44,33 @@ public class AuthenticationController {
             produces = MediaType.APPLICATION_JSON_VALUE
 
     )
+    @Operation(
+            summary = "Log into the system",
+            description = "Log into the system",
+            tags = {"Authentication"},
+            method = "POST"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TokenDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400", description = "Bad Request", content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "403", description = "Forbidden", content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "500", description = "Internal Error", content = @Content
+                    )
+            }
+    )
     public ResponseEntity<TokenDTO> login(@RequestBody AuthenticationDTO data) {
         UsernamePasswordAuthenticationToken usernamePassword =
                 new UsernamePasswordAuthenticationToken(data.getLogin(), data.getPassword());
@@ -51,6 +85,33 @@ public class AuthenticationController {
     @PostMapping(
             value = "/register",
             consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(
+            summary = "Register in the system",
+            description = "Register in the system",
+            tags = {"Authentication"},
+            method = "POST"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Created",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserResponseDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400", description = "Bad Request", content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "403", description = "Forbidden", content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "500", description = "Internal Error", content = @Content
+                    )
+            }
     )
     public ResponseEntity<UserResponseDTO> register(@RequestBody RegisterDTO data) {
         if (userService.findByLogin(data.getLogin()) != null) throw new LoginAlreadyUsedException("Login already in use");
