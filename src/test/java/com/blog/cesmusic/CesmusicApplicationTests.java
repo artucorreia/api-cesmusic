5e0e826a8253e6cceb31872b5081aa8b9f2d8433
artucorreia <115330684+artucorreia@users.noreply.github.com>
2024-08-17 06:35:46 -0300

    fix: configurar banco de dados de teste

--- src/test/java/com/blog/cesmusic/CesmusicApplicationTests.java
+++ src/test/java/com/blog/cesmusic/CesmusicApplicationTests.java
@@ -3,9 +3,15 @@ package com.blog.cesmusic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = CesmusicApplication.class)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.flyway.enabled=false"
+})
 class CesmusicApplicationTests {
 
 	@Test
