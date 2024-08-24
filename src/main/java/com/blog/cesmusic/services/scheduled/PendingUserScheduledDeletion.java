700595e6d43f19caadbb405a261dbb890c6b3db7
artucorreia <115330684+artucorreia@users.noreply.github.com>
2024-08-24 00:25:17 -0300

    fix

--- src/main/java/com/blog/cesmusic/services/scheduled/PendingUserScheduledDeletion.java
+++ src/main/java/com/blog/cesmusic/services/scheduled/PendingUserScheduledDeletion.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.logging.Logger;
 
 @Component
 public class PendingUserScheduledDeletion {
+    private final Logger logger = Logger.getLogger(PendingUserScheduledDeletion.class.getName());
+
     private final int EXPIRATION_TIME = 10;
 
     @Autowired
@@ -18,6 +21,8 @@ public class PendingUserScheduledDeletion {
     @Scheduled(fixedRate = 10000)
     @Transactional
     public void pendingUserDeletion() {
+        logger.info("Deleting pending users");
+
         LocalDateTime expirationTime = LocalDateTime.now().minusMinutes(EXPIRATION_TIME);
         repository.deleteAllByCreatedAtBefore(expirationTime);
     }
